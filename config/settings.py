919d9fa06be48ca64f468a83c51eb34cde7be163
Ruby Recto <ruby.recto@evsu.edu.ph>
2026-05-26 00:01:14 +0800

    Switch WhiteNoise storage to skip missing manifest files

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
